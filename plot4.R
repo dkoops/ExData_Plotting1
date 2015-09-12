4d79441e3a2e7abbca286b7a3c823ec809bab204
dkoops <david.koops@gmail.com>
2015-09-12 17:53:56 +1000

    Update to Plot4

--- plot4.R
+++ plot4.R
@@ -11,22 +11,28 @@ source(load_function)
 power_consumption <- load_power_consumption()
 
 # Multi plot a chart
-
-par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
-# plot 1 - Top Left
-plot(power_consumption$datetime, power_consumption$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
-# plot 2 - Top Right
-plot(power_consumption$datetime, power_consumption$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-# plot 3 - Bottome Left
-plot(power_consumption$datetime, power_consumption$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
-lines( power_consumption$datetime, power_consumption$Sub_metering_1, col ="black")
-lines( power_consumption$datetime, power_consumption$Sub_metering_2, col ="red")
-lines( power_consumption$datetime, power_consumption$Sub_metering_3, col = "blue")
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, lwd = 1, bty = "n")
-
-# plot 4 - Bottom Right
-plot(power_consumption$datetime, power_consumption$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
-
+png(file = "plot4.png", width = 480, height = 480)
+with(
+  power_consumption, {
+    par(mfrow = c(2, 2), mar = c(4, 4, 2, 1))
+    
+    # plot 1 - Top Left
+    plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+    
+    # plot 2 - Top Right
+    plot(datetime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+    
+    # plot 3 - Bottome Left
+    plot(datetime, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+    lines(datetime, Sub_metering_1, col = "black")
+    lines(datetime, Sub_metering_2, col = "red")
+    lines(datetime, Sub_metering_3, col = "blue")
+    legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, lwd = 1, bty = "n")
+    
+    # plot 4 - Bottom Right
+    plot(datetime, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+  }
+)
 # copy to png file
-dev.copy(png, file = "plot4.png", width = 480, height = 480)  ## Copy my plot to a PNG file
+#dev.copy(png, file = "plot4.png", width = 480, height = 480)  ## Copy my plot to a PNG file
 dev.off()  ## close the PNG device
